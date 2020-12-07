d7524b827f5b2f867a60a6e80c57bc86474f4b71
Michał Nowak <35508765+mnjmaverick@users.noreply.github.com>
2020-12-07 10:16:45 +0100

    Updated code for clarity

--- Day_1/Expense report.py
+++ Day_1/Expense report.py
@@ -4,4 +4,5 @@ from math import prod
 
 with open('Day_1/input.txt', 'r') as f:
     data = f.read().splitlines()
+    
 print(next(prod(x) for x in combinations([int(x) for x in data], 2) if sum(x) == 2020))
